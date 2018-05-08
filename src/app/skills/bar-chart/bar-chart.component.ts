3e51e09d4bbbf36f4dbc90c14a50c6ffd5441bfc
akshay.vyas <akshay.vyas@akshayvyass-MacBook-Pro.local>
2018-05-08 14:27:15 -0400

    chart component base

---
+++ src/app/skills/bar-chart/bar-chart.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bar-chart',
+  templateUrl: './bar-chart.component.html',
+  styleUrls: ['./bar-chart.component.css']
+})
+export class BarChartComponent implements OnInit {
+
+  constructor() { }
+
+  // var - provided JSON
+  skills = "";
+  experience = "";
+  user = "";
+
+  // var - testing
+
+  // INIT
+  ngOnInit() {
+  
+  }
+}
