4423fd85750517b1c01fde4d87395f3c4fd0f0e9
pedrum.golriz <pedrum.golriz@accenturefederal.com>
2018-05-09 09:49:15 -0400

    Faces structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { BarChartComponent } from './skills/bar-chart/bar-chart.component';
+import { FacesComponent} from './faces/faces.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BarChartComponent
+    BarChartComponent,
+    FacesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
