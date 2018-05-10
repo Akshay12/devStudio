d70c64763309f575305956529a0be7d86184eb21
chris.w.newman <chris.w.newman@accenturefederal.com>
2018-05-10 09:44:20 -0400

    some refactoring on face component to pass lint checks

--- src/app/faces/faces.component.ts
+++ src/app/faces/faces.component.ts
@@ -1,30 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { FaceService } from './face-service.service';
-
 @Component({
-  selector: 'our-faces',
+  selector: 'app-our-faces',
   templateUrl: './faces.component.html',
   styleUrls: ['./faces.component.css']
 })
 export class FacesComponent implements OnInit {
   faces: any;
   clickedFaces: any;
+
+  constructor(private faceService: FaceService) {}
+
   unclickFaces(index) {
-    for (let t in this.clickedFaces) {
-      if (index !== parseInt(t)) {
+    for (const t in this.clickedFaces) {
+      if (index !== parseInt(t, null)) {
         this.clickedFaces[t] = false;
       }
     }
   }
-  constructor(private faceService: FaceService) {}
 
   ngOnInit() {
     // retrieve from face service
     this.faceService.getFaces().subscribe(res => {
       this.faces = res;
-      if (res !== undefined && res.length > 0) {
-        this.clickedFaces = new Array(res.length);
-        for (var i in this.clickedFaces) {
+      if (this.faces !== undefined && this.faces.length > 0) {
+        this.clickedFaces = new Array(this.faces.length);
+        for (let i = 0; i < this.faces.length; i++) {
           this.clickedFaces[i] = false;
         }
       }
